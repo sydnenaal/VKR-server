56765aa8832a4f3d36e30f68ad9bf90f3d6a77ae
Dmitry Erokhin <dmitry.erokhin@alphaott.com>
2021-01-02 20:32:07 +0300

    VKRDEV-000: add koa framework

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('there')
+const Koa = require('koa')
+const chalk = require('chalk')
+const { logger } = require('./logger')
+
+const app = new Koa()
+
+const PORT = 3000
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World'
+})
+
+app.listen(PORT, () => {
+  console.clear()
+  console.log(chalk.blue(`Сервер доступен на порту ${PORT}`))
+  console.log(chalk.blue('Добро пожаловать!'))
+  console.log('\n')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,20 @@ module.exports = {
     compress: true,
     port: 9000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
